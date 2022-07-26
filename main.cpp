3009261179ff33e66438111106172dfd3327b7c8
이종학 <52825485+hak-2@users.noreply.github.com>
2022-07-26 23:27:29 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,23 +6,23 @@
 #include <arpa/inet.h>
 
 int main(int argc, char* argv[]) {
-	FILE *fp = fopen(argv[1], "r"){
-		if(fp == nullptr){
-			printf("fopen null [1] input!!!\n");
-			return 1;
-		}
+	FILE *fp = fopen(argv[1], "r");
+	if(fp == nullptr){
+		printf("fopen null [1] input!!!\n");
+		return 1;
 	}
+	
 	fread(&n, sizeof(n), 1, fp);
 
 	uint32_t n = ntohl(n);
 	fclose(fp);
 
-	FILE *fp = fopen(argv[2], "r"){
-		if(fp == nullptr){
-			printf("fopen null [2] input!!!\n");
-			return 1;
-		}
+	FILE *fp = fopen(argv[2], "r");
+	if(fp == nullptr){
+		printf("fopen null [2] input!!!\n");
+		return 1;
 	}
+	
 	fread(&n1, sizeof(n1), 1, fp);
 
 	uint32_t n1 = ntohl(n1);
